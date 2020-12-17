3c637b0e0acf8cd85d05bbab08bceeff33e0323c
stevenacz <scoailazaa@gmail.com>
2020-12-17 16:36:42 -0500

    Handling edge cases for our avg_score method

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from 'axios'
+import Airline from './Airline'
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
@@ -15,8 +16,13 @@ const Airlines = () => {
     .catch( resp => console.log(resp) )
   }, [airlines.length])
 
-  const list = airlines.map( item => {
-    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  const grid = airlines.map( item => {
+    return (
+      <Airline
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )
   })
 
   return (
@@ -26,7 +32,7 @@ const Airlines = () => {
         <div className="subheader">Honest, unbiased airline reviews.</div>
       </div>
       <div className="grid">
-        <ul>{list}</ul>
+        {grid}
       </div>
     </div>
   )
