84cf97766ae2e47b7181236cf236336d5255559e
stevenacz <scoailazaa@gmail.com>
2020-12-17 13:16:16 -0500

    Creating our API controllers

--- app/controllers/api/v1/airlines_controller.rb
+++ app/controllers/api/v1/airlines_controller.rb
@@ -1,14 +1,16 @@
 module Api
-  module v1
-    class AirlinesController < applicationController
+  module V1
+    class AirlinesController < ApplicationController
+      protect_from_forgery with: :null_session
+      
       def index
-        airlines = Airlines.all
+        airlines = Airline.all
 
         render json: AirlineSerializer.new(airlines, options).serialized_json
       end
 
       def show
-        airline = Airline.find_by(slug: param[:slug])
+        airline = Airline.find_by(slug: params[:slug])
 
         render json: AirlineSerializer.new(airline, options).serialized_json
       end
@@ -24,7 +26,7 @@ module Api
       end
 
       def update
-        airline = Airline.find_by(slug: param[:slug])
+        airline = Airline.find_by(slug: params[:slug])
 
         if airline.update(airline_params)
           render json: AirlineSerializer.new(airline, options).serialized_json
@@ -34,7 +36,7 @@ module Api
       end
 
       def destroy
-        airline = Airline.find_by(slug: param[:slug])
+        airline = Airline.find_by(slug: params[:slug])
 
         if airline.destroy
           head :no_content
